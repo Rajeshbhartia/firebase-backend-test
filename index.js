56d08754c1d1031f2075f15f5124763d5eaee8ac
Rajesh Bhartia <rbhartiamuk@gmail.com>
2020-12-27 11:30:38 +0600

    get and post request created for home route

--- index.js
+++ index.js
@@ -1,11 +1,26 @@
 var express = require('express');
+var cors = require('cors');
+const bodyParser = require('body-parser')
+
 var app = express();
 var PORT = 4000;
 
+app.use(cors())
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+
 app.get('/', function (req, res) {
-    res.send('Hello World!');
+    res.send({ qStatus: "successful" })
 });
 
+app.post('/', function (req, res) {
+    console.log(req.body)
+    res.send({ qStatus: "successful" })
+})
+
+
 app.listen(PORT, () => {
-    console.log(`server is listen on port ${PORT}`)
+    console.log(`server is listen on port http://localhost:${PORT}`)
 });
