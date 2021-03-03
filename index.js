908bd083ce90e320b476308e0f512202690b9fdc
Rajesh Bhartia <rbhartiamuk@gmail.com>
2021-03-03 17:20:41 +0600

    handle post queries for custom command

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ var cors = require('cors');
 const bodyParser = require('body-parser')
 const fileUpload = require('express-fileupload');
 fs = require('fs');
+const { exec } = require("child_process");
 
 var app = express();
 var PORT = 4000;
@@ -30,6 +31,50 @@ app.post('/uploadfile', function (req, res) {
         if (err) return console.log(err);
         console.log('Hello World > helloworld.txt');
     });
+});
+
+app.get('/getdirectory', function (req, res) {
+    console.log(req.body)
+    exec("ls", (error, stdout, stderr) => {
+        var directories = null;
+        if (error) {
+            console.log(`error: ${error.message}`);
+            return;
+        }
+        if (stderr) {
+            console.log(`stderr: ${stderr}`);
+            return;
+        }
+        directories = stdout.split(/\r?\n/)
+        console.log(`stdout: ${stdout}`);
+        console.log(typeof stdout);
+
+        res.send({ qStatus: "successful", directories });
+    });
+});
+
+app.post('/customCommand', function (req, res) {
+    console.log(req.body)
+
+    exec(req.body.command, (error, stdout, stderr) => {
+        var directories = null;
+        if (error) {
+            console.log(`error: ${error.message}`);
+            res.send({ qStatus: "unSuccessful", error: error.message });
+            return;
+        }
+        if (stderr) {
+            console.log(`stderr: ${stderr}`);
+            res.send({ qStatus: "unSuccessful", error: error.message });
+            return;
+        }
+
+        directories = stdout
+        console.log(`stdout: ${stdout}`);
+        res.send({ qStatus: "successful", resp: stdout });
+    });
+    // let s = parseInt(Math.random() * 100000000);
+    // res.send({ qStatus: "successful", token: 'token-' + s });
 });
 
 app.post('/signin', function (req, res) {
