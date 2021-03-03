cad170ebc9cb549342c900b0f9b73423d40a4f02
Rajesh Bhartia <rbhartiamuk@gmail.com>
2021-03-03 15:15:32 +0600

    file system added

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var cors = require('cors');
 const bodyParser = require('body-parser')
+const fileUpload = require('express-fileupload');
+fs = require('fs');
 
 var app = express();
 var PORT = 4000;
@@ -10,14 +12,24 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
     extended: true
 }));
+app.use(fileUpload());
 
 app.get('/', function (req, res) {
-    res.send({ qStatus: "successful" })
+    res.send("Hello from server")
 });
 
 app.post('/signup', function (req, res) {
     console.log(req.body)
     res.send({ qStatus: "successful", body: req.body, params: req.params });
+});
+
+app.post('/uploadfile', function (req, res) {
+    console.log(req.files.file)
+    res.send({ qStatus: "successful" });
+    fs.writeFile(`./public/${req.files.file.name}`, req.files.file.data, function (err) {
+        if (err) return console.log(err);
+        console.log('Hello World > helloworld.txt');
+    });
 });
 
 app.post('/signin', function (req, res) {
